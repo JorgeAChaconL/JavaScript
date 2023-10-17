158269d801473aa441d1910bcfcbdf80db828d2e
JorgeAChacon <chaconlealj@gmail.com>
2023-10-17 10:04:28 -0600

    canIVote() very basic logical function.
    Code Challenge JavaScript Fundamentals By CodeAcademy.
    The most common minimum age to vote is 18. Write a function canIVote() that takes in a number, representing the person’s age, and returns the boolean true if they are 18 years old or older, and the boolean false if they are not.

--- javaScript_Fundamentals/codeChallenge.js
+++ javaScript_Fundamentals/codeChallenge.js
@@ -1,4 +1,15 @@
-const greetWorld = () =>{
-    return `Hello, World!`
+const greetWorld = () => {
+  return `Hello, World!`;
+};
+
+const canIVote = (age) => {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
   }
-  
+};
+
+console.log(canIVote(19)); // Should print true
+console.log(canIVote(18)); // Should print true
+console.log(canIVote(17)); // Should print false
