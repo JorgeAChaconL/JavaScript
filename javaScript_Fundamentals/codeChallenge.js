22f030b78e025ba8eb45c7fff987f3510969cd71
JorgeAChacon <chaconlealj@gmail.com>
2023-10-18 18:55:54 -0600

    agreeOrDisagree()
    Code Challenge: JavaScript Fundamental By CodeAcademy.
    Write a function, agreeOrDisagree(), that takes in two strings, and returns 'You agree!' if the two strings are the same and 'You disagree!' if the two strings are different.
    
    Additionally division of functions declared on previous commits.

--- javaScript_Fundamentals/codeChallenge.js
+++ javaScript_Fundamentals/codeChallenge.js
@@ -1,7 +1,9 @@
+// greetWorld function, basic return message.
 const greetWorld = () => {
   return `Hello, World!`;
 };
 
+// CanIVote Function, basic if / else function
 const canIVote = (age) => {
   if (age >= 18) {
     return true;
@@ -13,3 +15,11 @@ const canIVote = (age) => {
 console.log(canIVote(19)); // Should print true
 console.log(canIVote(18)); // Should print true
 console.log(canIVote(17)); // Should print false
+
+// Function agreeOrDisagree, function comparative of two string and conditional check.
+const agreeOrDisagree = (string1, string2) =>{
+    return string1 === string2 ? 'You agree!': 'You disagree!'
+  }
+  
+  console.log(agreeOrDisagree("yep", "yep"))  // Should print 'You agree!'
+  console.log(agreeOrDisagree("yep", "yes")) // Should print 'You disagree!'
