8e4c2b91198b25bc84eced26cb0a12c138a1d9b5
JorgeAChacon <chaconlealj@gmail.com>
2023-10-18 20:19:42 -0600

    finalGrade() JavaScript Fundamentals.
    Code Challenge: JavaScript Fundamentals By CodeAcademy.
    Write a function, finalGrade(). It should:
    
    take three arguments of type number
    find the average of those three numbers
    return the letter grade (as a string) that the average corresponds to
    return ‘You have entered an invalid grade.’ if any of the three grades are less than 0 or greater than 100
    0-59 should return: 'F'
    60-69 should return: 'D'
    70-79 should return: 'C'
    80-89 should return: 'B'
    90-100 should return: 'A'

--- javaScript_Fundamentals/codeChallenge.js
+++ javaScript_Fundamentals/codeChallenge.js
@@ -17,30 +17,61 @@ console.log(canIVote(18)); // Should print true
 console.log(canIVote(17)); // Should print false
 
 // Function agreeOrDisagree, function comparative of two string and conditional check.
-const agreeOrDisagree = (string1, string2) =>{
-    return string1 === string2 ? 'You agree!': 'You disagree!'
-  }
-  
-  console.log(agreeOrDisagree("yep", "yep"))  // Should print 'You agree!'
-  console.log(agreeOrDisagree("yep", "yes")) // Should print 'You disagree!'
+const agreeOrDisagree = (string1, string2) => {
+  return string1 === string2 ? "You agree!" : "You disagree!";
+};
+
+console.log(agreeOrDisagree("yep", "yep")); // Should print 'You agree!'
+console.log(agreeOrDisagree("yep", "yes")); // Should print 'You disagree!'
 
 // Function lifePhase, fucntion with If / Else If statements.
-const lifePhase = age =>{
-    if (age >= 0 && age <=3){
-      return 'baby';
-    }else if(age >= 4 && age <= 12){
-      return 'child';
-    }else if(age >= 13 && age <= 19){
-      return 'teen';
-    }else if(age >= 20 && age <=64){
-      return 'adult';
-    }else if(age >= 65 && age <= 140){
-      return 'senior citizen';
-    }else{
-      return 'This is not a valid age';
-    }
+const lifePhase = (age) => {
+  if (age >= 0 && age <= 3) {
+    return "baby";
+  } else if (age >= 4 && age <= 12) {
+    return "child";
+  } else if (age >= 13 && age <= 19) {
+    return "teen";
+  } else if (age >= 20 && age <= 64) {
+    return "adult";
+  } else if (age >= 65 && age <= 140) {
+    return "senior citizen";
+  } else {
+    return "This is not a valid age";
   }
-  
-  // Uncomment the line below when you're ready to try out your function
-  console.log(lifePhase(5)) //should print 'child'
-  console.log(lifePhase(141)) //should print 'This is not a valid age'
+};
+
+console.log(lifePhase(5)); //should print 'child'
+console.log(lifePhase(141)); //should print 'This is not a valid age'
+
+// final grade, based in three different grades, take the average and evaluate Switch Case Statements.
+const finalGrade = (grd1, grd2, grd3) => {
+  if (
+    grd1 < 0 ||
+    grd1 > 100 ||
+    grd2 < 0 ||
+    grd2 > 100 ||
+    grd3 < 0 ||
+    grd3 > 100
+  ) {
+    return "You have entered an invalid grade.";
+  }
+
+  const avg = (grd1 + grd2 + grd3) / 3;
+
+  switch (true) {
+    case avg >= 90:
+      return "A";
+    case avg >= 80:
+      return "B";
+    case avg >= 70:
+      return "C";
+    case avg >= 60:
+      return "D";
+    default:
+      return "F";
+  }
+};
+
+console.log(finalGrade(99, 92, 95)); // Should print 'A'
+console.log(finalGrade(99, 92, 0)); // Should print 'A'
