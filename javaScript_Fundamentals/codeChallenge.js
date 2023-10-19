dd0ccb35ab57dc55ff00bd8716ce697b4e0c6a13
JorgeAChacon <chaconlealj@gmail.com>
2023-10-18 19:15:38 -0600

    Function lifePhase.
    JavaScript Fundamentals: Code Challenges By CodeAcademy.
    Write a function, lifePhase(), that takes in a person’s age, as a number, and returns which phase of life they are in.
    
    Here are the classifications:
    0-3 should return 'baby'
    4-12 should return 'child'
    13-19 should return 'teen'
    20-64 should return 'adult'
    65-140 should return 'senior citizen'
    If the number is less than 0 or greater than 140, the program should return 'This is not a valid age'

--- javaScript_Fundamentals/codeChallenge.js
+++ javaScript_Fundamentals/codeChallenge.js
@@ -23,3 +23,24 @@ const agreeOrDisagree = (string1, string2) =>{
   
   console.log(agreeOrDisagree("yep", "yep"))  // Should print 'You agree!'
   console.log(agreeOrDisagree("yep", "yes")) // Should print 'You disagree!'
+
+// Function lifePhase, fucntion with If / Else If statements.
+const lifePhase = age =>{
+    if (age >= 0 && age <=3){
+      return 'baby';
+    }else if(age >= 4 && age <= 12){
+      return 'child';
+    }else if(age >= 13 && age <= 19){
+      return 'teen';
+    }else if(age >= 20 && age <=64){
+      return 'adult';
+    }else if(age >= 65 && age <= 140){
+      return 'senior citizen';
+    }else{
+      return 'This is not a valid age';
+    }
+  }
+  
+  // Uncomment the line below when you're ready to try out your function
+  console.log(lifePhase(5)) //should print 'child'
+  console.log(lifePhase(141)) //should print 'This is not a valid age'
