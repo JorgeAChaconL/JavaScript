2baf9769951d0ab33515684dd3f19fec4e8624ce
JorgeAChacon <chaconlealj@gmail.com>
2023-12-08 14:39:43 -0600

    Secret Message Challenge Complete
    Functions completed with the requirements of the tasks given.
    
    By Jorge A Chacon L

--- arraysChallenges/secretMessage.js
+++ arraysChallenges/secretMessage.js
@@ -25,3 +25,35 @@ let secretMessage = [
   "Learn",
   "JavaScript",
 ];
+
+//Usean array method to remove the las string of the array.
+secretMessage.pop();
+
+//Check your work by logging `lenght` of the array.
+console.log(`Lenght of the array: ${secretMessage.length}`);
+
+//Use an array metho to add words `to` and `programs`
+secretMessage.push('to','Program');
+
+//Change word 'easily' to 'right' by accesing index and replacing it.
+  //Function find the index of the word 'easily'
+const findEasily = secretMessage.findIndex(element => element =='easily');
+console.log(`Index of 'easily': ${findEasily}`);
+  //replace the word in the index and check.
+secretMessage[20] = 'right';
+console.log(secretMessage[20]);
+
+//use an array method to remove first string on it.
+secretMessage.shift()
+
+//Array method to add the string 'Programming' to the beginnig.
+secretMessage.unshift('Programming');
+
+//Array method to remov strings `get`,`right`,`the`,`first`,`time`, and replace with the single string `know`;
+  //Function find the index of the word 'get'
+const findGet = secretMessage.findIndex(element => element =='get');
+console.log(`Index of 'get': ${findGet}`);
+secretMessage.splice(6,5,'know');
+
+//One line, use console.log() and join() ot print secretMessage sentence. Join(separator) to avoid `,` in the message.
+console.log(secretMessage.join(' '));
