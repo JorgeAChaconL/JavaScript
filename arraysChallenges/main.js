0d2960d4bd10190cf994eb68a931e558f46e2a6f
JorgeAChacon <chaconlealj@gmail.com>
2023-12-11 11:44:29 -0600

    Reverse For Loop
    Make a for loop that loops backwards printing 3 to 0 to the console. Use the >= comparison operator in your stopping condition and the -- operator in your iteration statement.

--- arraysChallenges/main.js
+++ arraysChallenges/main.js
@@ -2,3 +2,12 @@
 for(let i = 5; i < 11; i++){
     console.log(i);
   }
+
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 0; counter < 4; counter++){
+    console.log(counter);
+  }
+  //Reverse loop
+  for(let i = 3; i >= 0; i--){
+    console.log(i);
+  }
